b73cd556e098ed7c59adc5dfc0e13a566f0ca2d5
Hirthuk <sharankumarp02@gmail.com>
2024-08-31 23:02:29 +0530

    css practice'

--- src/Componenets/Header.jsx
+++ src/Componenets/Header.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { useState } from "react";
-import style from './Header.module.css'
+import style from './Header.module.css';
 
-function Header(props){
-    return <div className= {style.container}> 
-        <h1 id={style.heading}>Food Booker</h1>
-    </div>
+function Header(props) {
+    return (
+        <div className={style.container}>
+            <img id={style.image} src="" alt="myImage" />
+            <h1 id={style.heading}>Food Booker</h1>
+            
+                <button id={style.buttonElement} type="button">DarkLight</button>
+                <button id={style.buttonElement} type="button">Profile</button>
+            
+        </div>
+    );
 }
 
 export default Header;
