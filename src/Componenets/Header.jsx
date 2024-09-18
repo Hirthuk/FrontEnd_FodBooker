7974b4ca8144d1d0d7942cbb2f2547f247463efe
Hirthuk <sharankumarp02@gmail.com>
2024-09-18 13:11:34 +0530

    css learned

--- src/Componenets/Header.jsx
+++ src/Componenets/Header.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import style from './Header.module.css';
+import logo from './../assets/android-chrome-192x192.png'
 
 function Header(props) {
     return (
         <div className={style.container}>
-            <img id={style.image} src="" alt="myImage" />
+            <img id={style.image} src={logo} alt="myImage" />
+            <img id={style.image} src={logo} alt="myImage" width={200} height={100}/>
             <h1 id={style.heading}>Food Booker</h1>
-            
+                <div className="buttoncontainer">
                 <button id={style.buttonElement} type="button">DarkLight</button>
                 <button id={style.buttonElement} type="button">Profile</button>
-            
+                </div>
         </div>
     );
 }
